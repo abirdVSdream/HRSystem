9b37dc2b3014d13711700c164582843bc836cc30
abirdVSdream <cainiao20150728@163.com>
2016-03-02 14:39:18 +0800

    Add HrException

---
+++ src/exception/HrException.java
@@ -0,0 +1,43 @@
+package exception;
+
+/**
+ * <p>Title: HrException.java</p>
+ * <p>Description: </p>
+ * @author  caizelin
+ * @date 2016年3月2日
+ * @version  1.0
+ */
+
+public class HrException extends Exception {
+
+	public HrException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HrException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HrException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HrException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HrException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 根据标识属性来
+	 * @param 
+	 * @return 
+	 */
+}
+
